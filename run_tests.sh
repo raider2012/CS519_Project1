64219270a0e46f564b3a0fbb1247276117b4f93b
raghvesh <raghvesh@node-0.rp1399linux.rise-pg0.utah.cloudlab.us>
2025-02-19 05:37:06 +0000

    First Run

--- run_tests.sh
+++ run_tests.sh
@@ -12,11 +12,12 @@
 
 # Default buffer sizes to test if none are provided
 if [ "$#" -eq 0 ]; then
-    buffer_sizes=(256 512 1024 2048)
+    buffer_sizes=(256 512 1024 2048 4096 8192 16384)
 else
     buffer_sizes=("$@")
 fi
 
+loops=(10 50 100 500 1000)
 # Compile the test_app_helper program if the binary doesn't exist.
 if [ ! -f test_app_helper ]; then
     echo "Compiling test_app_helper.c..."
@@ -30,10 +31,12 @@ fi
 # Run the test_app_helper program for each buffer size.
 for size in "${buffer_sizes[@]}"; do
     echo "--------------------------------------------------"
-    echo "Running test with buffer size: ${size} bytes"
-    ./test_app_helper ${size}
+    for iterations in "${loops[@]}"; do
+	echo "Running test with buffer size: ${size} bytes and iterations ${iterations}"
+    	./test_app_helper ${size} ${iterations}
+	echo "--------"
+    done
     echo ""
 done
 
 echo "All tests completed."
-
